ebe0f2405f93705210b6b8b4ebd0d4f08f46e2d6
Agustin Lozano Blua <agustinn.lozanoo@hotmail.com>
2021-08-15 12:50:05 -0300

    Agrega cambios para la resolucion del juego Hasta el momento el programa solo podia inicializar un tablero mediante un objeto que lo representara, pero visualmente no teniamos nada.
    A partir de estos cambios, ademas de eso, se utiliza el objeto en conjunto
    con la funcion mezclarTablero para configurar uno en la partida.
    
    Es decir, una vez que inicialicemos el programa, generaremos el tablero
    con una configuracion de cuadros aleatoria, de tal manera que cada partida
    tenga un tablero unico.

--- src/js/game.js
+++ src/js/game.js
@@ -15,6 +15,14 @@ function iniciarTablero(tablero) {
     obtenerCuadros(tablero);
 }
 
+//Mezclar tablero
+function mezclarTablero(tablero) { 
+    const indicesMezclados = mezclarNumerosEntre(0, 15);
+    const cuadrosMezclados = mezclarCuadros(tablero, indicesMezclados);
+
+    tablero.elementos = cuadrosMezclados;
+}
+
 function obtenerPersonajes(tablero) {
     tablero['personajes'] = [
         'Aragorn', 
@@ -30,59 +38,65 @@ function obtenerPersonajes(tablero) {
 
 function obtenerColores(tablero) {
     tablero['colores'] = [
-        'rojo',
-        'azul',
-        'amarillo',
-        'violeta',
-        'verde',
-        'rosa',
-        'naranja',
-        'marron'
+        'color-1',
+        'color-2',
+        'color-3',
+        'color-4',
+        'color-5',
+        'color-6',
+        'color-7',
+        'color-8',
+        'color-1',
+        'color-2',
+        'color-3',
+        'color-4',
+        'color-5',
+        'color-6',
+        'color-7',
+        'color-8'
     ]
 }
 
 function obtenerCuadros(tablero) {
-    tablero['elementos-ordenados'] = document.querySelectorAll('.cuadro');
+    tablero['elementos'] = document.querySelectorAll('.cuadro');
 }
 
-
-//Mezclar tablero
-function mezclarTablero(tablero) {
-    obtenerIndicesNulos(tablero);
-    mezclarArray(tablero.indices);
-}
-
-function obtenerIndicesNulos(tablero) {
-    const NUMERO_CUADROS = 16, VALOR_NULO = -1;
-
-    tablero['indices'] = [];
-
-    for (let i = 0; i < NUMERO_CUADROS; i++) {
-        tablero['indices'][i] = VALOR_NULO;
-    }
-}
-
-function mezclarArray(indices) {
+function mezclarNumerosEntre(minimo, maximo) {
     let posicion = 0;
 
-    while (posicion < 16) {
-        let numeroAleatorio = generarNumeroEntre(1, 16);
+    const array = obtenerArrayNulo(tablero);
+
+    while (posicion < array.length) {
+        let numeroAleatorio = generarNumeroEntre(minimo, maximo);
         let count = 0;
 
-        for (let i = 0; i<indices.length; i++) {
-            if (numeroAleatorio === indices[i]) {
+        for (let i = 0; i<array.length; i++) {
+            if (numeroAleatorio === array[i]) {
                 break;
             } else {
                 count++;
             }
 
-            if (count === indices.length-1) {
-                indices[posicion] = numeroAleatorio;
+            if (count === array.length-1) {
+                array[posicion] = numeroAleatorio;
                 posicion++;
                 break;
             }
         }
     }
+
+    return array;
+}
+
+function obtenerArrayNulo() {
+    const NUMERO_INDICES = 16, VALOR_NULO = null;
+    const array = [];
+
+    for (let i = 0; i < NUMERO_INDICES; i++) {
+        array[i] = VALOR_NULO;
+    }
+
+    return array;
 }
 
 function generarNumeroEntre(minimo, maximo) {
@@ -90,5 +104,63 @@ function generarNumeroEntre(minimo, maximo) {
     return numero;
 }
 
+function mezclarCuadros(tablero, indices) { 
+    const colores = tablero['colores'];
+    const coloresMezclados = mezclarColores(colores, indices);
+
+    configurarCuadro(tablero, coloresMezclados);
+
+    return tablero.elementos;
+}
+
+function mezclarColores(colores, indices) {
+    const coloresMezclados = [];
+
+    indices.forEach((indice, i) => {
+        coloresMezclados[i] = colores[indice]
+    });
+
+    return coloresMezclados;
+}
+
+function configurarCuadro(tablero, colores) {
+    const cuadros = tablero.elementos;
+    const personajes = tablero.personajes
+
+    cuadros.forEach((cuadro, i) => {
+        if (colores[i] === 'color-1') {
+            cuadro.className = `col cuadro ${colores[i]}`;
+            cuadro.innerHTML = `${personajes[0]}`
+        }
+        if (colores[i] === 'color-2') {
+            cuadro.className = `col cuadro ${colores[i]}`;
+            cuadro.innerHTML = `${personajes[1]}`
+        }
+        if (colores[i] === 'color-3') {
+            cuadro.className = `col cuadro ${colores[i]}`;
+            cuadro.innerHTML = `${personajes[2]}`
+        }
+        if (colores[i] === 'color-4') {
+            cuadro.className = `col cuadro ${colores[i]}`;
+            cuadro.innerHTML = `${personajes[3]}`
+        }
+        if (colores[i] === 'color-5') {
+            cuadro.className = `col cuadro ${colores[i]}`;
+            cuadro.innerHTML = `${personajes[4]}`
+        }
+        if (colores[i] === 'color-6') {
+            cuadro.className = `col cuadro ${colores[i]}`;
+            cuadro.innerHTML = `${personajes[5]}`
+        }
+        if (colores[i] === 'color-7') {
+            cuadro.className = `col cuadro ${colores[i]}`;
+            cuadro.innerHTML = `${personajes[6]}`
+        }
+        if (colores[i] === 'color-8') {
+            cuadro.className = `col cuadro ${colores[i]}`;
+            cuadro.innerHTML = `${personajes[7]}`
+        }
+    });
+}
 
 iniciarPartida();
